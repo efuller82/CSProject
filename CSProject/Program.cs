0410ee5efbdbeab173e850299e871c2f531867e9
Eric Fuller <efuller82@gmail.com>
2020-01-01 14:33:18 -0600

    Add project files.

---
+++ CSProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSProject
+{
+
+    
+    class Staff
+    {
+        private float hourlyRate;
+        private int hWorked;
+        public static CalculatePay()
+        {
+            return hWorked * hourlyRate;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
