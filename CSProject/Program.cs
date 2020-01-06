221fb34b720a9dfac028415ede385c14e2e064c7
Eric Fuller <efuller82@gmail.com>
2020-01-05 21:23:25 -0600

    Staff class created

--- CSProject/Program.cs
+++ CSProject/Program.cs
@@ -11,10 +11,45 @@ namespace CSProject
     {
         private float hourlyRate;
         private int hWorked;
-        public static CalculatePay()
+        
+        public float TotalPay { get; protected set; }
+        public float BasicPay { get; private set; }
+        public string NameOfStaff { get; private set; }
+        public int HoursWorked
         {
-            return hWorked * hourlyRate;
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (value > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
         }
+
+        public Staff(string name, float rate)
+        {
+            NameOfStaff = name;
+            hourlyRate = rate;
+        }
+
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Calculating Pay...");
+
+            BasicPay = hWorked * hourlyRate;
+
+            BasicPay = TotalPay;
+        }
+
+        public override string ToString()
+        {
+            return "hourlyRate = " + hourlyRate + ", hworked = " + hWorked + ", Name of Staff = " + NameOfStaff; 
+        }
+
     }
     class Program
     {
